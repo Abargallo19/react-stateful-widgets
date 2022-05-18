eaa4d1c40430ce0261ef9f8f29585785db3b8849
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-05-18 19:15:02 -0400

    input works fine

--- src/components/Input.js
+++ src/components/Input.js
@@ -41,12 +41,14 @@ import { useState } from 'react';
 export default function Input() {
   /* STEP 1 */
 const [inputValue, setInputValue] = useState("");
+
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
 
     /* STEP 4 */
+    setInputValue(value)
   };
   const reset = () => {
     /* STEP 5 */
@@ -62,7 +64,7 @@ const [inputValue, setInputValue] = useState("");
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}></div> {/* STEP 3 */}
+      <div id='output' style={style}>{inputValue.toUpperCase()}</div> {/* STEP 3 */}
       <div>
         <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
