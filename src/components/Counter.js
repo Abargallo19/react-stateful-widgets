12241007e7427c8ec4f2832791330d00f5af8afd
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-05-18 09:49:38 -0400

    reset button set to 0

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -52,7 +52,7 @@ import React, {useState} from 'react'; /* STEP 0 */
 export default function Counter() {
   /* STEP 1 */
   const [count, setCount] = useState(0);
-
+console.log(count)
   const increment = () => {
     /* STEP 4 */
     setCount(count + 1);
@@ -63,6 +63,7 @@ export default function Counter() {
   };
   const reset = () => {
     /* STEP 6 */
+    setCount(count);
   };
 
   const style = {
@@ -75,7 +76,7 @@ export default function Counter() {
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number 0 is even {/* STEP 3 */}
+        Number {count} is even {/* STEP 3 */}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
