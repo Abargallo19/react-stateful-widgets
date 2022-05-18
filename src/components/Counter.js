2c1013c522be5ca830080c548e8857144763d25d
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-05-18 09:34:56 -0400

    imported useState, set increment and decrement

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,17 +46,20 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
+// import { useState } from 'react';
 
 export default function Counter() {
   /* STEP 1 */
-  const [count, setCount] = countState(0);
+  const [count, setCount] = useState(0);
 
   const increment = () => {
     /* STEP 4 */
+    setCount(count + 1);
   };
   const decrement = () => {
     /* STEP 5 */
+    setCount(count - 1);
   };
   const reset = () => {
     /* STEP 6 */
